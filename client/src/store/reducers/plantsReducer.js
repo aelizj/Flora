bbec9bd699a05f605e88864c7a802af3c9b9abca
Anne Jones <anne1365@outlook.com>
2023-05-11 15:32:21 -0700

    plants reducer

--- client/src/store/reducers/plantsReducer.js
+++ client/src/store/reducers/plantsReducer.js
@@ -4,8 +4,33 @@ const initialState = {
   error: null
 };
 
-const plantsReducer = () => {
+const plantsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_PLANTS_REQUEST':
+      return {
+        ...state,
+        loading: true
+      };
 
+    case 'FETCH_PLANTS_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        plantsList: action.payload,
+        error: null
+      };
+
+    case 'FETCH_PLANTS_FAILURE':
+      return {
+        ...state,
+        loading: false,
+        plantsList: [],
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default plantsReducer;
