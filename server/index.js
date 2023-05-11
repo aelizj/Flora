1a4f81d7a29ae6c2d5b5a88b5e1d92991c1a7243
Anne Jones <anne1365@outlook.com>
2023-05-10 17:09:28 -0700

    tidy up file

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import apiRoutes from './routes/api';
-import uiRoutes from './routes/ui';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.static(`${__dirname}/public`));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use((err, next) => {
+  console.log(err);
+  next();
+});
 
 mongoose
   .connect(process.env.DB, {
@@ -17,16 +25,6 @@ mongoose
   })
   .then(() => console.log('Database connected successfully.'))
   .catch((err) => console.error(err));
-
-app.use(cors());
-app.use(express.static(`${__dirname}/public`));
-app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/ui', uiRoutes);
-app.use((err, next) => {
-  console.log(err);
-  next();
-});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
