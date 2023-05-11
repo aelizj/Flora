2605ea017b751789c7e8e283a6d5293e285206a2
Anne Jones <anne1365@outlook.com>
2023-05-11 12:56:53 -0700

    Add store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,3 +1,11 @@
-const store = true;
+import { configureStore, applyMiddleware, combineReducers } from "@reduxjs/toolkit";
+import thunk from 'redux-thunk';
+import plantsReducer from './reducers/plantsReducer';
+
+const rootReducer = combineReducers({
+  plants: plantsReducer,
+})
+
+const store = configureStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
