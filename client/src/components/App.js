48d9a8486db8f6ad0b97c219d541f22d3160a68a
Anne Jones <anne1365@outlook.com>
2023-05-11 15:33:39 -0700

    Remove logo

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,7 @@
-import logo from '../assets/images/logo.svg';
-import '../assets/stylesheets/App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
