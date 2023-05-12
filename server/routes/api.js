851a24411ca6c7ca279713366b3dfef7ce7d59d3
Anne Jones <anne1365@outlook.com>
2023-05-12 16:17:19 -0700

    add plants endpoint to db from server

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import Plant from '../models/plant';
 
 const router = express.Router();
+
+router.get('/api/plants', async (req, res) => {
+  try {
+    const plants = await Plant.find();
+    res.json(plants);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 export default router;
