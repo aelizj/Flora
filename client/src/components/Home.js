649dc3e63485ce7fd67c4f2e8bf6b79061dbe256
Anne Jones <anne1365@outlook.com>
2023-05-12 18:21:59 -0700

    home page component

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h3>Welcome to Flora!</h3>
+      <p>Explore, learn, and share with other plant enthusiasts.</p>
+    </div>
+  );
+};
+
+export default Home;
