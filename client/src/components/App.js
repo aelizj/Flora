7b15a964f960401456c388f84414cc39b5c9fa37
Anne Jones <anne1365@outlook.com>
2023-05-12 18:20:25 -0700

    App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,28 @@
-import logo from '../assets/images/icon.svg';
 import '../assets/stylesheets/App.css';
-
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
+import Home from './Home'
+import PlantList from './PlantList'
+import Footer from './Footer';
+import Community from './Community';
+import Profile from './Profile'
+;
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Flora!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p></p>
+    <Router>
+      <Header />
+      <Navigation />
+      <Routes>
+        <Route path="/plants" element={<PlantList />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/community" element={<Community />} />
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
