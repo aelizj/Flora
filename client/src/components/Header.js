e36b1f8d5dc3f891ab7b58a1564b8eb6efbc8a18
Anne Jones <anne1365@outlook.com>
2023-05-12 18:22:19 -0700

    header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import logo from '../assets/images/icon.svg';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Flora</h1>
+    </header>
+  );
+};
+
+export default Header;
