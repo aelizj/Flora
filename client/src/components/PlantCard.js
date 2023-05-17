496b0dc0357312c748231d400e982fd443afaad9
Anne Jones <anne1365@outlook.com>
2023-05-17 14:00:21 -0700

    plant card component

---
+++ client/src/components/PlantCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PlantCard = ({ plant }) => {
+  return (
+    <div className='plant-card'>
+      <p></p>
+      <div>
+
+      </div>
+    </div>
+  );
+};
+
+export default PlantCard;
