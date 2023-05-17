ea0999ee2f4210835ee327bf73fcf5a8ba2a9138
Anne Jones <anne1365@outlook.com>
2023-05-17 14:00:53 -0700

    Plant info component

---
+++ client/src/components/PlantInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlantInfo = () => {
+  return (
+    <div className='plant-basics'>
+      <p>Plant Basics</p>
+    </div>
+  );
+};
+
+export default PlantInfo;
