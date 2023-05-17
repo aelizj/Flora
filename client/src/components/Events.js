5ae83fea232360e100f6c18e2021d1e91ca8f33b
Anne Jones <anne1365@outlook.com>
2023-05-17 13:59:50 -0700

    events component

---
+++ client/src/components/Events.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Event from './Event.js';
+
+
+const Events = () => {
+  return (
+    <div className='events'>
+      <p>Events</p>
+      <div>
+        {/* map through events and send each to Event component */}
+      </div>
+    </div>
+  );
+};
+
+export default Events;
