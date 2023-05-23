752d91f1f60878535e9d9b099ba105a84cac8b89
Anne Jones <anne1365@outlook.com>
2023-05-23 15:08:29 -0700

    Added plant data

--- client/src/lib/data.js
+++ client/src/lib/data.js
@@ -1,12 +1,12 @@
 const plantData = [
   {
-    commonName: 'Monstera, Swiss cheese plant',
+    commonName: 'Split-leaf philodendron',
     scientificName: 'Monstera deliciosa',
     imageUrl: 'https://images.unsplash.com/photo-1625582598943-2445bb7b8253?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1035&q=80',
-    description: 'Monstera deliciosa, also known as the Swiss cheese plant, is a popular houseplant native to the tropical rainforests of Central and South America. It is loved for its unique, large, glossy leaves with natural holes, or fenestrations.'
+    description: 'Not actually a member of the philodendron family, the Monstera deliciosa, also known as the Swiss cheese plant, is a popular houseplant native to the tropical rainforests of Central and South America. It is loved for its unique, large, glossy leaves with natural holes, or fenestrations.'
   },
   {
-    commonName: 'Heartleaf philodendron',
+    commonName: 'Brasil philodendron',
     scientificName: 'Philodendron brasil',
     imageUrl: 'https://images.unsplash.com/photo-1600411833196-7c1f6b1a8b90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80',
     description: 'The Philodendron Brasil, also known as Philodendron hederaceum \'Brasil\' or Philodendron scandens \'Brasil,\' is a popular tropical houseplant that belongs to the Araceae family. It is a cultivar of the species Philodendron hederaceum, native to the rainforests of Central and South America.'
@@ -46,7 +46,26 @@ const plantData = [
     scientificName: 'Zamioculcas zamiifolia',
     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Zamioculcas.jpg',
     description: 'Zamioculcas is genus of flowering plants in the family Araceae, containing the single species Zamioculcas zamiifolia. It is a tropical perennial plant, native to eastern Africa, from southern Kenya to northeastern South Africa.'
+  },
+  {
+    commonName: 'Chinese Money Plant',
+    scientificName: 'Pilea peperomioides',
+    imageUrl: 'https://images.unsplash.com/photo-1589999251554-21789097351f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=696&q=80',
+    description: 'Pilea peperomioides, the Chinese money plant, UFO plant, pancake plant, lefse plant or missionary plant, is a species of flowering plant in the nettle family Urticaceae, native to Yunnan and Sichuan provinces in southern China. Commonly confused as a member of the Peperomia family.'
+  },
+  {
+    commonName: 'Zebra Haworthia',
+    scientificName: 'Haworthiopsis attenuata',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Ab_plant_846.jpg',
+    description: 'Haworthiopsis attenuata, formerly Haworthia attenuata, commonly known as zebra haworthia, is a small species of succulent plant from the Eastern Cape Province, South Africa. As an ornamental, it is one of the most commonly cultivated of the Haworthiopsis species.'
+  },
+  {
+    commonName: 'Philodendron xanadu',
+    scientificName: 'Thaumatophyllum xanadu',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Philodendron_xanadu_AK2.jpg',
+    description: 'Thaumatophyllum xanadu is a perennial plant belonging to the arum family Araceae and the genus Thaumatophyllum, formerly classified under the Meconostigma subgenus of Philodendron. This plant is native to Brazil, but is widely cultivated as a landscape plant in tropical, subtropical and warm temperate climates.'
   }
+
 ];
 
 export default plantData;
