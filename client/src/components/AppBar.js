86555e690bd5adbf5568e2f063c0b9e1d04c063d
Anne Jones <anne1365@outlook.com>
2023-05-24 17:01:42 -0700

    Account icon on app bar

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,11 +1,15 @@
 import { drawerWidth } from '../constants/UiValues';
-import React from 'react';
+import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Box from '@mui/material/Box';
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
@@ -24,23 +28,68 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-const FloraAppBar = ({ drawerOpen, handleDrawerOpen }) => (
-  <AppBar position="fixed" open={drawerOpen}>
-    <Toolbar>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={handleDrawerOpen}
-        edge="start"
-        sx={{ mr: 2, ...(drawerOpen && { display: 'none' }) }}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h5" noWrap component="div">
-        Flora
-      </Typography>
-    </Toolbar>
-  </AppBar>
-);
+const FloraAppBar = ({ drawerOpen, handleDrawerOpen }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box>
+      <AppBar position="fixed" open={drawerOpen}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(drawerOpen && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" noWrap component="div">
+            Flora
+          </Typography>
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
 export default FloraAppBar;
