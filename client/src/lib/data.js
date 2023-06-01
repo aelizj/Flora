477c4e7ea75c5f0b6dd9264b6ea21e435d9eb6e2
Anne Jones <anne1365@outlook.com>
2023-06-01 16:28:04 -0700

    modify dummy data

--- client/src/lib/data.js
+++ client/src/lib/data.js
@@ -2,79 +2,85 @@ const plantData = [
   {
     commonName: 'Split-leaf philodendron',
     scientificName: 'Monstera deliciosa',
-    imageUrl: 'https://images.unsplash.com/photo-1625582598943-2445bb7b8253?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1035&q=80',
+    imageUrl: '/assets/images/monstera.png',
     description: 'Not actually a member of the philodendron family, the Monstera deliciosa, also known as the Swiss cheese plant, is a popular houseplant native to the tropical rainforests of Central and South America. It is loved for its unique, large, glossy leaves with natural holes, or fenestrations.'
   },
-  {
-    commonName: 'Brasil philodendron',
-    scientificName: 'Philodendron brasil',
-    imageUrl: 'https://images.unsplash.com/photo-1600411833196-7c1f6b1a8b90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80',
-    description: 'The Philodendron Brasil, also known as Philodendron hederaceum \'Brasil\' or Philodendron scandens \'Brasil,\' is a popular tropical houseplant that belongs to the Araceae family. It is a cultivar of the species Philodendron hederaceum, native to the rainforests of Central and South America.'
-  },
-  {
-    commonName: 'Rabbit\'s foot fern',
-    scientificName: 'Davallia fejeensis',
-    imageUrl: 'https://images.mobot.org/PlantRecordsVol2/prod/large960/00005000/5835_B587-0120040.jpg',
-    description: 'Rabbit\'s Foot Fern (Davallia fejeensis) is a unique and interesting fern variety known for its furry, creeping rhizomes that resemble a rabbit\'s foot. This tropical plant prefers conditions similar to its native habitat in the Fiji Islands and requires appropriate care to thrive.'
-  },
+
   {
     commonName: 'Silver sword philodendron',
     scientificName: 'Philodendron hastatum',
-    imageUrl: 'https://live.staticflickr.com/65535/51282015899_cf10f7bf2a_b.jpg',
+    imageUrl: '/assets/images/silverSword.png',
     description: 'Native to Fiji, the Silver Sword Philodendron, scientifically known as Philodendron hastatum, is a popular houseplant due to its unique silvery, metallic-looking foliage. '
   },
   {
     commonName: 'Golden pothos, Devil\'s ivy',
     scientificName: 'Epipremnum aureum',
-    imageUrl: 'https://bs.plantnet.org/image/o/d56087a265bed821a33d2f20ad22338071152272',
+    imageUrl: '/assets/images/goldenPothos.png',
     description: 'Epipremnum aureum is a species in the arum family Araceae, native to Mo\'orea in the Society Islands of French Polynesia'
   },
   {
     commonName: 'Snake plant',
     scientificName: 'Dracaena trifasciata',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Snake_Plant_%28Sansevieria_trifasciata_%27Laurentii%27%29.jpg',
+    imageUrl: '/assets/images/snakePlant.png',
     description: 'Snake plant is an erect, clumping, broadleaf evergreen houseplant in the Asparagaceae (asparagus) family. It is native to Africa.'
   },
-  {
-    commonName: 'Spider plant',
-    scientificName: 'Chlorophytum comosum',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Spider_plant_with_plantlets_and_flowers.jpg',
-    description: 'Chlorophytum comosum, usually called spider plant or common spider plant due to its spider-like look, also known as spider ivy, ribbon plant, and hen and chickens is a species of evergreen perennial flowering plant of the family Asparagaceae.'
-  },
-  {
-    commonName: 'ZZ plant',
-    scientificName: 'Zamioculcas zamiifolia',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Zamioculcas.jpg',
-    description: 'Zamioculcas is genus of flowering plants in the family Araceae, containing the single species Zamioculcas zamiifolia. It is a tropical perennial plant, native to eastern Africa, from southern Kenya to northeastern South Africa.'
-  },
+
   {
     commonName: 'Chinese Money Plant',
     scientificName: 'Pilea peperomioides',
-    imageUrl: 'https://images.unsplash.com/photo-1589999251554-21789097351f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=696&q=80',
+    imageUrl: '/assets/images/moneyPlant.png',
     description: 'Pilea peperomioides, the Chinese money plant, UFO plant, pancake plant, lefse plant or missionary plant, is a species of flowering plant in the nettle family Urticaceae, native to Yunnan and Sichuan provinces in southern China. Commonly confused as a member of the Peperomia family.'
   },
   {
     commonName: 'Zebra Haworthia',
     scientificName: 'Haworthiopsis attenuata',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Ab_plant_846.jpg',
+    imageUrl: '/assets/images/zebraSucculent.png',
     description: 'Haworthiopsis attenuata, formerly Haworthia attenuata, commonly known as zebra haworthia, is a small species of succulent plant from the Eastern Cape Province, South Africa. As an ornamental, it is one of the most commonly cultivated of the Haworthiopsis species.'
   },
-  {
-    commonName: 'Philodendron xanadu',
-    scientificName: 'Thaumatophyllum xanadu',
-    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Philodendron_xanadu_AK2.jpg',
-    description: 'Thaumatophyllum xanadu is a perennial plant belonging to the arum family Araceae and the genus Thaumatophyllum, formerly classified under the Meconostigma subgenus of Philodendron. This plant is native to Brazil, but is widely cultivated as a landscape plant in tropical, subtropical and warm temperate climates.'
-  }
+
 
 ];
 
 export default plantData;
 
-
+// Template -------------------------------------
 
 // {
 //   commonName: '',
 //   scientificName: '',
 //   imageUrl: '',
 //   description: ''
+// }
+
+// Extra dummy data -----------------------------
+
+// {
+//   commonName: 'ZZ plant',
+//   scientificName: 'Zamioculcas zamiifolia',
+//   imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Zamioculcas.png',
+//   description: 'Zamioculcas is genus of flowering plants in the family Araceae, containing the single species Zamioculcas zamiifolia. It is a tropical perennial plant, native to eastern Africa, from southern Kenya to northeastern South Africa.'
+// },
+// {
+//   commonName: 'Brasil philodendron',
+//   scientificName: 'Philodendron brasil',
+//   imageUrl: 'https://images.unsplash.com/photo-1600411833196-7c1f6b1a8b90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1364&q=80',
+//   description: 'The Philodendron Brasil, also known as Philodendron hederaceum \'Brasil\' or Philodendron scandens \'Brasil,\' is a popular tropical houseplant that belongs to the Araceae family. It is a cultivar of the species Philodendron hederaceum, native to the rainforests of Central and South America.'
+// },
+// {
+//   commonName: 'Rabbit\'s foot fern',
+//   scientificName: 'Davallia fejeensis',
+//   imageUrl: 'https://images.mobot.org/PlantRecordsVol2/prod/large960/00005000/5835_B587-0120040.png',
+//   description: 'Rabbit\'s Foot Fern (Davallia fejeensis) is a unique and interesting fern variety known for its furry, creeping rhizomes that resemble a rabbit\'s foot. This tropical plant prefers conditions similar to its native habitat in the Fiji Islands and requires appropriate care to thrive.'
+// },
+// {
+//   commonName: 'Spider plant',
+//   scientificName: 'Chlorophytum comosum',
+//   imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Spider_plant_with_plantlets_and_flowers.png',
+//   description: 'Chlorophytum comosum, usually called spider plant or common spider plant due to its spider-like look, also known as spider ivy, ribbon plant, and hen and chickens is a species of evergreen perennial flowering plant of the family Asparagaceae.'
+// },
+// {
+//   commonName: 'Philodendron xanadu',
+//   scientificName: 'Thaumatophyllum xanadu',
+//   imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Philodendron_xanadu_AK2.png',
+//   description: 'Thaumatophyllum xanadu is a perennial plant belonging to the arum family Araceae and the genus Thaumatophyllum, formerly classified under the Meconostigma subgenus of Philodendron. This plant is native to Brazil, but is widely cultivated as a landscape plant in tropical, subtropical and warm temperate climates.'
 // }
