25d21d2ac9fe40dbd0f43da40a2ed89f5b09af41
Anne Jones <anne1365@outlook.com>
2023-06-05 21:02:33 -0700

    refactor db connection

--- server/index.js
+++ server/index.js
@@ -9,22 +9,30 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
 
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected successfully.');
+  } catch (err) {
+    console.error('Error connecting to database: ', err);
+  }
+};
+
+connectToDb();
+
 app.use(cors());
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 app.use('/api', apiRoutes);
-app.use((err, next) => {
-  console.log(err);
-  next();
+app.use((err, req, res) => {
+  console.error(err);
+  res.status(err.status || 500).json({ message: err.message });
 });
 
-mongoose
-  .connect(process.env.DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Database connected successfully.'))
-  .catch((err) => console.error('Error connecting to database: ', err));
+connectToDb();
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
