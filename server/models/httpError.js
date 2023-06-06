a7035851e66f8364642416f2c851c1c4587a0ad2
Anne Jones <anne1365@outlook.com>
2023-06-05 21:48:25 -0700

    additional debugging functionality

--- server/models/httpError.js
+++ server/models/httpError.js
@@ -1,7 +1,9 @@
 class HttpError extends Error {
-  constructor(error, errorCode) {
-    super(error);
-    this.code = errorCode;
+  constructor(message, errorCode, details = null) {
+    super(message);
+    this.status = errorCode || 500;
+    this.details = details; // For adding additional details about the error
+    Error.captureStackTrace(this, this.constructor); // Creates a .stack property
   }
 }
 
