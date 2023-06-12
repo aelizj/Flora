32679baf57e29505c440c6403cda68c8c04e3111
Anne Jones <anne1365@outlook.com>
2023-06-12 14:53:27 -0700

    add route  to get plant by id

--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { PLANTS_INDEX_URL } from "../constants/ApiRoutes";
 
-const API_BASE_URL = 'http://localhost:5001'
+const API_BASE_URL = 'http://localhost:5001';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
@@ -12,18 +12,27 @@ const getPlants = async () => {
   try {
     const response = await apiClient.get(PLANTS_INDEX_URL);
     return response.data.plants;
-  } catch(err) {
+  } catch (err) {
     throw(err);
   }
-}
+};
 
 const createPlant = async (newPlant) => {
   try {
     const response = await apiClient.post(PLANTS_INDEX_URL, { plant: newPlant });
     return response.data;
-  } catch(err) {
+  } catch (err) {
     throw(err);
   }
-}
+};
 
-export { getPlants, createPlant };
+const getPlantById = async (id) => {
+  try {
+    const response = await apiClient.get(`/plants/${id}`);
+    return response.data.Plant;
+  } catch (err) {
+    throw(err);
+  }
+};
+
+export { getPlants, createPlant, getPlantById };
