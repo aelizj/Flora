fb3b8f0f81a95ac9a473ac081f3609afae972851
Anne Jones <anne1365@outlook.com>
2023-06-13 15:42:53 -0700

    add user model

---
+++ server/models/user.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'An email is needed to submit.'],
+  },
+  password: {
+    type: String,
+    required: [true, 'A password is needed to submit.'],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  achievements: {
+    type: Array,
+    default: [],
+    required: false,
+  },
+  plantCollection: {
+    type: Array,
+    default: [],
+    required : false,
+  },
+  wishlist: {
+    type: Array,
+    default: [],
+    required: false,
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+export default User;
