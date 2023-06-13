6d08d4260d0e52f6dbac6a07e0334ef469a8e0bc
Anne Jones <anne1365@outlook.com>
2023-06-12 20:03:35 -0700

    individual plant page

--- client/src/components/pages/PlantList/Plant.js
+++ client/src/components/pages/PlantList/Plant.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import ReactMarkdown from 'react-markdown';
 import { useParams } from 'react-router-dom';
 import { getPlantById } from '../../../store/features/plant';
+import { Box, Card, CardMedia, Container, Grid, Typography } from '@mui/material';
 
 const Plant = () => {
   const { id } = useParams();
@@ -17,12 +19,56 @@ const Plant = () => {
   if (!plant) return <p>No plant data!</p>
 
   return (
-    <div className='plant'>
-      <h6 className='plant-title'>{plant.commonName}</h6>
-      <img src={plant.imageUrl} alt={plant.commonName} className='plant-image'/>
-      <p className='plant-description'>{plant.description}</p>
-      <h4 className='plant-care-guide'>{plant.careGuide}</h4>
-    </div>
+    <Container maxWidth="100%" disableGutters>
+      <Container sx={{ p:2 }} className='plantGuide'>
+       <Box div className='plant-guide-header'>
+       <Typography variant="h2" color="primary">
+        {plant.commonName}
+       </Typography>
+       <Typography variant="h5" color="secondary" sx={{ fontStyle: 'italic' }}>
+         ({plant.scientificName})
+       </Typography>
+       </Box>
+
+       <Box div className='plant-guide-content'>
+         <Grid container spacing={6} sx={{ mt: 1 }}>
+           <Grid item xs="6">
+             <Box sx={{}}>
+               <Typography variant="h5" color="grey">
+                 Description
+               </Typography>
+               <Typography variant="body" color="black">
+                 <ReactMarkdown>{plant.description}</ReactMarkdown>
+               </Typography>
+
+               <Typography variant="h5" color="grey">
+                 Caring for {plant.commonName}s
+               </Typography>
+               <Typography variant="body1" color="black">
+                 <ReactMarkdown>{plant.careGuide}</ReactMarkdown>
+               </Typography>
+             </Box>
+           </Grid>
+
+           <Grid item xs={6}>
+             <Box>
+               <Card>
+                 <CardMedia
+                   component="img"
+                   alt={'Picture of ' + plant.commonName}
+                   sx={{ width: '100%', height: 'auto'}}
+                   image={plant.imageUrl}
+                 />
+               </Card>
+             </Box>
+           </Grid>
+         </Grid>
+        </ Box>
+      </Container>
+
+
+    </Container>
+
   );
 };
 
