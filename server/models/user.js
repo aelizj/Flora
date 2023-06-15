749605e9eb1602365e76eb2aa39d3d5a1ce77896
Anne Jones <anne1365@outlook.com>
2023-06-14 20:24:12 -0700

    add fields to user model

--- server/models/user.js
+++ server/models/user.js
@@ -3,19 +3,29 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A name is required'],
+    required: [true, 'A name is needed to submit.'],
   },
   email: {
     type: String,
-    required: [true, 'An email is needed to submit.'],
+    required: [true, 'An email is required to submit.'],
   },
   password: {
     type: String,
     required: [true, 'A password is needed to submit.'],
   },
+  username: {
+    type: String,
+    unique: true,
+    required: [true, 'A username is needed to submit'],
+  },
   date: {
     type: Date,
     default: Date.now,
+  },
+  interests: {
+    type: Array,
+    default: [],
+    required: false,
   },
   achievements: {
     type: Array,
