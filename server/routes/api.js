59acbf45e97ef7b97af59601a97adf5e03d9a4f7
Anne Jones <anne1365@outlook.com>
2023-06-14 20:24:00 -0700

    add login and register routes to router

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { registerUser, loginUser } from '../controllers/authController.js';
 import { getPlants, createPlant, getPlantById } from '../controllers/plantsController.js';
 
 const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 router.get('/plants', getPlants);
 router.post('/plants', createPlant);
