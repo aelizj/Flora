da498dae1e9c6d3b5c98d180b2b2f5ae998dacbc
Anne Jones <anne1365@outlook.com>
2023-06-19 13:30:11 -0700

    add auth to redux store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./features/auth";
 import plantsReducer from './features/plants';
 import plantReducer from './features/plant';
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     plants: plantsReducer,
     plant: plantReducer,
   }});
