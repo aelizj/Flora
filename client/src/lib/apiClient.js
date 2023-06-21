a7935e2f728401bd537c4d314be04b5f64167d07
Anne Jones <anne1365@outlook.com>
2023-06-20 20:10:20 -0700

    add login and register endpoints

--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -1,19 +1,36 @@
 import axios from "axios";
-import { PLANTS_INDEX_URL } from "../constants/ApiRoutes";
+import { LOGIN_USER_URL, REGISTER_USER_URL, PLANTS_INDEX_URL } from "../constants/ApiRoutes";
 
 const API_BASE_URL = 'http://localhost:5001';
-
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
 });
 
+const loginUser = async (loginData) => {
+  try {
+    const response = await apiClient.post(LOGIN_USER_URL, loginData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const registerUser = async (userData) => {
+  try {
+    const response = await apiClient.post(REGISTER_USER_URL, userData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 const getPlants = async () => {
   try {
     const response = await apiClient.get(PLANTS_INDEX_URL);
     return response.data.plants;
-  } catch (err) {
-    throw(err);
+  } catch (error) {
+    throw error;
   }
 };
 
@@ -21,8 +38,8 @@ const createPlant = async (newPlant) => {
   try {
     const response = await apiClient.post(PLANTS_INDEX_URL, { plant: newPlant });
     return response.data;
-  } catch (err) {
-    throw(err);
+  } catch (error) {
+    throw error;
   }
 };
 
@@ -30,9 +47,9 @@ const getPlantById = async (id) => {
   try {
     const response = await apiClient.get(`${PLANTS_INDEX_URL}/${id}`);
     return response.data.plant;
-  } catch (err) {
-    throw(err);
+  } catch (error) {
+    throw error;
   }
 };
 
-export { getPlants, createPlant, getPlantById };
+export { loginUser, registerUser, getPlants, createPlant, getPlantById };
