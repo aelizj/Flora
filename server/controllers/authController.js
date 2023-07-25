5b743f609d0c7641d83c2426203925b33a2d7506
Anne Jones <anne1365@outlook.com>
2023-07-25 15:31:45 -0700

    authController changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -40,6 +40,16 @@ const validateToken = async (req, res, next) => {
 const registerUser = async (req, res, next) => {
   console.log('Inside registerUser function');
 
+  if (
+    !req.body.firstName
+    || !req.body.lastName
+    || !req.body.username
+    || !req.body.email
+    || !req.body.password
+  ) {
+    return next(new HttpError('All fields are required for submission.', 400));
+  }
+
   const existingUser = await User.findOne({ email: req.body.email });
   if (existingUser) return next(new HttpError('That email is already in use.', 409));
 
@@ -47,7 +57,8 @@ const registerUser = async (req, res, next) => {
   if (usernameInUse) return next(new HttpError('That username is already in use', 409));
 
   const newUser = new User({
-    name: req.body.name,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
@@ -56,25 +67,50 @@ const registerUser = async (req, res, next) => {
   const salt = await bcrypt.genSalt(10);
   newUser.password = await bcrypt.hash(newUser.password, salt);
 
-  const user = await newUser.save();
+  let user;
+
+  try {
+    user = await newUser.save();
+  } catch (err) {
+    return next(new HttpError(`Error occurred during registration: ${err}`, 500));
+  }
 
   return createTokenAndSetCookie(user, res, next);
 };
 
 // Validates entered user credentials
 const loginUser = async (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    return next(new HttpError('All fields are required for submission.', 400));
+  }
+
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
     if (!user) return next(new HttpError('No account associated with this email address exists.', 404));
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return next(new HttpError('Incorrect password.', 400));
+    if (!isMatch) return next(new HttpError('Incorrect password.', 403));
 
     return createTokenAndSetCookie(user, res, next);
   } catch (err) {
-    return next(new HttpError('Error occured during login', 500));
+    return next(new HttpError(`Error occured during login: ${err}`, 500));
   }
 };
+
+// Logs out user
+const logoutUser = async (req, res, next) => {
+  if (!req.cookies.jwt) {
+    return next(new HttpError('Bad request', 400));
+  }
+
+  try {
+
+
+  } catch (err) {
+    return next(new HttpError(`Error occured during logout: ${err}`, 500));
+  }
+};
+
 
 export { validateToken, registerUser, loginUser };
