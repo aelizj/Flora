6f5f3a44d9819fee5cd8f576d0b6fa7deef8385c
Anne Jones <anne1365@outlook.com>
2023-07-25 16:32:50 -0700

    api client route additions

--- client/src/constants/ApiRoutes.js
+++ client/src/constants/ApiRoutes.js
@@ -1,4 +1,6 @@
-export const PLANTS_INDEX_URL = '/api/plants';
-export const LOGIN_USER_URL = '/api/login';
-export const REGISTER_USER_URL = '/api/register';
 export const VALIDATE_TOKEN_URL = '/api/validate-token';
+export const LOGIN_USER_URL     = '/api/login';
+export const LOGOUT_USER_URL    = '/api/logout'
+export const REGISTER_USER_URL  = '/api/register';
+export const PLANTS_INDEX_URL   = '/api/plants';
+export const USERS_INDEX_URL    = '/api/users';
--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -1,33 +1,39 @@
 import axios from "axios";
-import { VALIDATE_TOKEN_URL, LOGIN_USER_URL, REGISTER_USER_URL, PLANTS_INDEX_URL } from "../constants/ApiRoutes";
+import { VALIDATE_TOKEN_URL, LOGIN_USER_URL, LOGOUT_USER_URL, REGISTER_USER_URL, PLANTS_INDEX_URL, USERS_INDEX_URL } from "../constants/ApiRoutes";
 
 const API_BASE_URL = 'http://localhost:5001';
 axios.defaults.withCredentials = true;
+
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
 });
 
 const validateToken = async () => {
-  console.log('inside client validateToken function')
-
   try {
     const response = await apiClient.get(VALIDATE_TOKEN_URL);
     console.log(response.data);
     return response.data;
-  } catch (error) {
-    throw error;
+  } catch (err) {
+    throw err;
   }
 };
 
 const loginUser = async (loginData) => {
-  console.log('inside client loginUser function')
-
   try {
     const response = await apiClient.post(LOGIN_USER_URL, loginData);
     return response.data;
-  } catch (error) {
-    throw error;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const logoutUser = async () => {
+  try {
+    const response = await apiClient.post(LOGOUT_USER_URL);
+    return response.data;
+  } catch (err) {
+    throw err;
   }
 };
 
@@ -35,19 +41,17 @@ const registerUser = async (userData) => {
   try {
     const response = await apiClient.post(REGISTER_USER_URL, userData);
     return response.data;
-  } catch (error) {
-    throw error;
+  } catch (err) {
+    throw err;
   }
 };
 
 const getPlants = async () => {
-  console.log('inside client getPlants function')
-
   try {
     const response = await apiClient.get(PLANTS_INDEX_URL);
     return response.data.plants;
-  } catch (error) {
-    throw error;
+  } catch (err) {
+    throw err;
   }
 };
 
@@ -55,19 +59,46 @@ const createPlant = async (newPlant) => {
   try {
     const response = await apiClient.post(PLANTS_INDEX_URL, { plant: newPlant });
     return response.data;
-  } catch (error) {
-    throw error;
+  } catch (err) {
+    throw err;
   }
 };
 
 const getPlantById = async (id) => {
-  console.log('inside client getPlantById function')
   try {
     const response = await apiClient.get(`${PLANTS_INDEX_URL}/${id}`);
     return response.data.plant;
-  } catch (error) {
-    throw error;
+  } catch (err) {
+    throw err;
   }
 };
 
-export { validateToken, loginUser, registerUser, getPlants, createPlant, getPlantById };
+const getUsers = async () => {
+  try {
+    const response = await apiClient.get(USERS_INDEX_URL);
+    return response.data.users;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    const response = await apiClient.get(`${USERS_INDEX_URL}/${id}`);
+    return response.data.user;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export {
+  validateToken,
+  loginUser,
+  logoutUser,
+  registerUser,
+  getPlants,
+  createPlant,
+  getPlantById,
+  getUsers,
+  getUserById,
+};
