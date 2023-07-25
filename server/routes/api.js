3567d750b8d628a2f608c208321c8cf0c8d78f59
Anne Jones <anne1365@outlook.com>
2023-07-25 15:32:25 -0700

    add routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import passport from 'passport';
-import { validateToken, registerUser, loginUser } from '../controllers/authController.js';
+import { validateToken, registerUser, loginUser, logoutUser } from '../controllers/authController.js';
 import { getPlants, createPlant, getPlantById } from '../controllers/plantsController.js';
+import { getUsers, getUserById, deleteUserById } from '../controllers/usersController.js';
 
 const router = express.Router();
 
 router.get('/validate-token', validateToken);
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
-router.get('/plants', getPlants);
-router.post('/plants', passport.authenticate('jwt', { session: false }), createPlant);
 router.get('/plants/:id', passport.authenticate('jwt', { session: false }), getPlantById);
+router.post('/plants', passport.authenticate('jwt', { session: false }), createPlant);
+router.get('/plants', getPlants);
+
+router.get('/users/:id', getUserById);
+router.get('/users', getUsers);
+router.delete('users/{id}', deleteUserById);
 
 export default router;
