169f0b5af94c06b74a3f7901f81c41af389559e0
Anne Jones <anne1365@outlook.com>
2023-07-26 16:43:08 -0700

    made section rendering conditional

--- client/src/components/pages/Profile/UserAchievements.js
+++ client/src/components/pages/Profile/UserAchievements.js
@@ -1,25 +1,31 @@
-import { Typography, Stack, Chip } from '@mui/material';
+import { Box, Typography, Stack, Chip } from '@mui/material';
 import { teal } from '@mui/material/colors';
 
 const UserAchievements = ({ achievements }) => {
-  return (
-    <>
-      <Typography variant="h6" sx={{ mt: 2 }}>
-        Achievements
-      </Typography>
-      <Stack direction="row" spacing={1} mt={1} flexWrap="wrap">
-        {achievements.map((achievement, index) => (
-          <Chip
-            key={index}
-            label={achievement}
-            color="success"
-            variant="outlined"
-            sx={{ bgcolor: teal[50] }}
-          />
-        ))}
-      </Stack>
-    </>
-  );
+  if (achievements && achievements.length > 0) {
+    return (
+      <>
+        <Typography variant="h6" sx={{ mt: 2 }}>
+          Achievements
+        </Typography>
+        <Stack direction="row" spacing={1} mt={1} flexWrap="wrap">
+          {achievements.map((achievement, index) => (
+            <Chip
+              key={index}
+              label={achievement}
+              color="success"
+              variant="outlined"
+              sx={{ bgcolor: teal[50] }}
+            />
+          ))}
+        </Stack>
+      </>
+    );
+  } else {
+    return (
+      <Box sx={{ p:2 }} />
+    )
+  }
 }
 
 export default UserAchievements;
