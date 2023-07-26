2b9946f43d90a29845beb2f8c9286217906abd25
Anne Jones <anne1365@outlook.com>
2023-07-26 16:43:22 -0700

    made section rendering conditional

--- client/src/components/pages/Profile/UserCard.js
+++ client/src/components/pages/Profile/UserCard.js
@@ -1,16 +1,18 @@
 import { Paper, Typography, Stack, Chip } from '@mui/material';
 
 const UserCard = ({ category, items }) => {
-  return (
-    <Paper elevation={2} sx={{ padding: 2 }}>
-      <Typography variant="h6">{category}</Typography>
-      <Stack direction="row" spacing={1} mt={1} flexWrap="wrap">
-        {items.map((item, i) => (
-          <Chip key={i} label={item} color="primary" />
-        ))}
-      </Stack>
-    </Paper>
-  );
+  if (items) {
+    return (
+      <Paper elevation={2} sx={{ padding: 2 }}>
+        <Typography variant="h6">{category}</Typography>
+        <Stack direction="row" spacing={1} mt={1} flexWrap="wrap">
+          {items.map((item, i) => (
+            <Chip key={i} label={item} color="primary" sx={{ mb:1, fontWeight: 600 }}/>
+          ))}
+        </Stack>
+      </Paper>
+    );
+  }
 }
 
 export default UserCard;
