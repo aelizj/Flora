b197feb10276695b22fc9e258c845f311d8ebbf5
Anne Jones <anne1365@outlook.com>
2023-07-28 15:22:33 -0700

    form validation

--- client/src/components/pages/PlantList/AddPlantDialog.js
+++ client/src/components/pages/PlantList/AddPlantDialog.js
@@ -15,18 +15,17 @@ import {
  import AddRoundedIcon from '@mui/icons-material/AddRounded';
  import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
  import FlatButton from '../../ui/FlatButton';
+import { useForm } from 'react-hook-form';
+
 
 const AddPlantDialog = () => {
   const dispatch = useDispatch();
   const [dialogOpen, setDialogOpen] = React.useState(false);
-
-  const [newPlant, setNewPlant] = useState({
-    commonName: '',
-    scientificName: '',
-    imageUrl: '',
-    description: '',
-    careGuide: '',
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
 
   const handleClickOpenDialog = () => {
     setDialogOpen(true);
@@ -36,12 +35,8 @@ const AddPlantDialog = () => {
     setDialogOpen(false);
   };
 
-  const handleInputChange = (e) => {
-    setNewPlant({ ...newPlant, [e.target.id]: e.target.value });
-  };
-
-  const handleSubmit = () => {
-    dispatch(createPlant(newPlant));
+  const onSubmit = (data) => {
+    dispatch(createPlant(data));
     handleCloseDialog();
   };
 
@@ -82,50 +77,73 @@ const AddPlantDialog = () => {
           </DialogContentText>
 
           <TextField // common name
+            {...register('commonName', {
+              required: 'Common name is required',
+              maxLength: {
+                value: 140,
+                message: "Common name may not be longer than 140 characters"
+              }
+            })}
             id='commonName'
             label='Common Name'
-            onChange={handleInputChange}
-            value={newPlant.commonName}
             autoFocus
             required
             fullWidth
             margin='dense'
             color='primary'
             size='small'
-            helperText="Plant's colloquial name; i.e. Chinese money plant"
+            error={!!errors.commonName}
+            helperText={errors.commonName? errors.commonName.message : "Plant's colloquial name; i.e. Chinese money plant"}
           />
 
           <TextField // scientific name
+            {...register('scientificName', {
+              required: 'Scientific name is required',
+              maxLength: {
+                value: 140,
+                message: "Scientific name may not be longer than 140 characters"
+              }
+            })}
             id='scientificName'
             label='Scientific Name'
-            onChange={handleInputChange}
-            value={newPlant.scientificName}
             autoFocus
             required
             fullWidth
             margin='dense'
             color='primary'
             size='small'
-            helperText="Plant's binomial scientific name; i.e. Pilea peperomioides"
+            error={!!errors.scientificName}
+            helperText={errors.scientificName? errors.scientificName.message : "Plant's binomial scientific name; i.e. Pilea peperomioides"}
           />
 
          <TextField // imageUrl
+            {...register('imageUrl', {
+              maxLength: {
+                value: 250,
+                message: "Image URL may not be longer than 250 characters"
+              }
+            })}
             id='imageUrl'
             label='Image URL'
-            onChange={handleInputChange}
-            value={newPlant.imageUrl}
             autoFocus
             fullWidth
             margin='dense'
             color='primary'
             size='small'
+            error={!!errors.imageUrl}
+            helperText={errors.imageUrl?.message}
+
           />
 
           <TextField // description
+            {...register('description', {
+              maxLength: {
+                value: 2500,
+                message: "Description may not be longer than 2,500 characters"
+              }
+            })}
             id='description'
             label='Description'
-            onChange={handleInputChange}
-            value={newPlant.description}
             autoFocus
             multiline
             fullWidth
@@ -134,13 +152,19 @@ const AddPlantDialog = () => {
             minRows={2}
             maxRows={4}
             size='small'
+            error={!!errors.description}
+            helperText={errors.description?.message}
           />
 
           <TextField // care guide
+            {...register('careGuide', {
+              maxLength: {
+                value: 10000,
+                message: "Care guide may not be longer than 5,000 characters"
+              }
+            })}
             id='careGuide'
             label='Care Guide'
-            onChange={handleInputChange}
-            value={newPlant.careGuide}
             autoFocus
             multiline
             fullWidth
@@ -149,12 +173,14 @@ const AddPlantDialog = () => {
             minRows={4}
             maxRows={8}
             size='small'
+            error={!!errors.careGuide}
+            helperText={errors.careGuide?.message}
           />
         </DialogContent>
 
         <DialogActions>
           <FlatButton size="small" variant="outlined" onClick={handleCloseDialog} startIcon={<DeleteRoundedIcon/>}>Cancel</FlatButton>
-          <FlatButton color="success" size="small" variant="contained" onClick={handleSubmit} endIcon={<AddRoundedIcon/>}>Add</FlatButton>
+          <FlatButton color="success" size="small" variant="contained" onClick={handleSubmit(onSubmit)} endIcon={<AddRoundedIcon/>}>Add</FlatButton>
         </DialogActions>
       </Dialog>
     </Box>
