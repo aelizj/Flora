b8b98d4a3bec1819c51bc75ac73ae4cdc758db86
Anne Jones <anne1365@outlook.com>
2023-07-28 17:03:57 -0700

    refactor

--- client/src/components/ui/DrawerComponent.js
+++ client/src/components/ui/DrawerComponent.js
@@ -1,18 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import React from 'react';
-import { drawerWidth, mainColor } from '../../constants/UiValues';
-import NavLinks from '../../constants/NavLinks';
 import { styled } from '@mui/material/styles';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
+import { Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { drawerWidth, mainColor } from '../../constants/UiValues';
+import NavLinks from '../../constants/NavLinks';
 import DrawerHeader from './DrawerHeader';
 
 const StyledNavLink = styled(NavLink)({
--- client/src/components/ui/Footer.js
+++ client/src/components/ui/Footer.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import { Box, Container, Typography } from '@mui/material';
 
 const StickyFooter = () => {
   return (
