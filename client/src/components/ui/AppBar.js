4563b8f2f75b1a1b9f05946b9eebc0d20681179b
Anne Jones <anne1365@outlook.com>
2023-07-28 17:51:10 -0700

    typo

--- client/src/components/ui/AppBar.js
+++ client/src/components/ui/AppBar.js
@@ -1,20 +1,17 @@
+import { drawerWidth } from '../../constants/UiValues';
 import { useSelector, useDispatch } from 'react-redux';
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 import { logoutUser } from '../../store/features/user';
-import { drawerWidth } from '../../constants/UiValues';
-import {
-  MuiAppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  MenuIcon,
-  MenuItem,
-  Menu
-} from '@mui/material';
-
 
 
 const AppBar = styled(MuiAppBar, {
