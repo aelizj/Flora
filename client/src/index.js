dc1d9db162bdceaed16407642ee0cbf0ef14c806
Anne Jones <anne1365@outlook.com>
2023-09-07 18:52:31 -0700

    rename theme

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './App';
-import testTheme from './assets/themes/theme';
+import floraTheme from './assets/themes/theme';
 
-const theme = testTheme;
+const theme = floraTheme;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 console.log('Inside client-side index.js file');
