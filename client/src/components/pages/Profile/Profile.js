432e12ca36e6dc0ac86454a7d6c4d11817d66a7c
Anne Jones <anne1365@outlook.com>
2024-01-29 15:50:26 -0800

    add authored guides to profile

--- client/src/components/pages/Profile/Profile.js
+++ client/src/components/pages/Profile/Profile.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Box, Container, Grid, } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import UserAvatar        from './UserAvatar.js';
 import UserInfo          from './UserInfo';
 import UserCard          from './UserCard';
 import UserAchievements  from './UserAchievements';
 import CoverPhoto        from './CoverPhoto.js';
 import EditProfileDialog from './EditProfileDialog.js';
+import PlantGuideCard    from '../PlantGuideList/PlantGuideCard.js';
 import UserBio from './UserBio.js';
 
 const ProfilePage = () => {
   const { user } = useSelector((state) => state.user);
+
+  console.log(user);
   return (
     <div>
       {user.coverImageUrl ? <CoverPhoto user={user}/> : <Box sx={{ padding: 4 }}/>}
@@ -21,10 +24,11 @@ const ProfilePage = () => {
             flexDirection: 'column',
             alignItems: 'center',
             minHeight: 'fit-content',
+            paddingBottom: 20
           }}
         >
           <UserAvatar avatar={user.profileImageUrl} />
-          <UserInfo name={`${user.firstName} ${user.lastName}`} email={user.email} location={user.location} />
+          <UserInfo name={`${user.firstName} ${user.lastName}`} username={user.username} location={user.location} />
           <Grid
             container
             direction="row"
@@ -40,8 +44,27 @@ const ProfilePage = () => {
                 />
               </Grid>
             ))}
+
+            {user.authoredPlantGuides &&(
+              <Grid item xs={12} sm={12} paddingTop={6}>
+                <Typography variant='h6' color="primary">
+                  Authored Plant Guides
+                </Typography>
+              </Grid>
+            )}
+
+
+            {user.authoredPlantGuides && user.authoredPlantGuides.map(plantGuide => (
+              <Grid item xs={4} key={plantGuide.id}>
+                <PlantGuideCard plantGuide={plantGuide}/>
+              </Grid>
+             ))}
           </Grid>
-          { user.authoredPlantGuides ? user.authoredPlantGuides.join(' | ') : ''} [// TODO: Deleted plant guide not removed from user's profile (GitHub Issue #18)]
+          <div>
+            <Grid container spacing={0}>
+
+            </Grid>
+          </div>
           { user.achievements ? <UserAchievements achievements={user.achievements} /> : <></> }
           <EditProfileDialog user={user}/>
         </Box>
