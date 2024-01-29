9c0efa9da86c3e110716a6664d04336513afae7f
Anne Jones <anne1365@outlook.com>
2024-01-29 15:51:03 -0800

    style changes

--- client/src/components/pages/Profile/UserInfo.js
+++ client/src/components/pages/Profile/UserInfo.js
@@ -1,22 +1,22 @@
 import { Typography } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-const UserInfo = ({ name, email, location }) => {
+const UserInfo = ({ name, username, location }) => {
   return (
     <>
       <Typography
-        sx={{ fontWeight: 'bold', fontSize: { xs: 'h6.fontSize', sm: 'h4.fontSize' } }}
-        variant="h5"
+        sx={{ fontWeight: 'bold', fontSize: { xs: 'h4.fontSize', sm: 'h3.fontSize' }, paddingBottom: 0.5 }}
+        variant="h2"
       >
         {name}
       </Typography>
 
       <Typography
         color="textSecondary"
-        variant="body1"
-        sx={{ fontSize: { xs: 'body2.fontSize', sm: 'body1.fontSize' } }}
+        variant="h6"
+        sx={{ fontSize: { xs: 'h6.fontSize', sm: 'h5.fontSize' }, fontWeight: 400, }}
       >
-        {email}
+        {`@${username}`}
       </Typography>
 
       <Typography
